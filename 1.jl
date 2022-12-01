71efdec0fe700c0d4da0d8076912cf4efc9dec08
Alexander Rauhut <alexraw@mailbox.org>
2022-12-01 20:26:39 +0100

    final :)

--- 1.jl
+++ 1.jl
@@ -1,3 +1,7 @@
-per_elf(file) = map(x -> sum(parse.(Int, split(x))), split(readchomp(file), "\n\n"))
-sum_max_n(file, k) = sum(partialsort!(per_elf(file), k, rev=true))
-println(sum_max_n("data/1.txt", 1), '\n', sum_max_n("data/1.txt", 1:3))
+function solve(file::String)
+    per_elf = map(x -> sum(parse.(Int, split(x))), split(readchomp(file), "\n\n"))
+    top3 = partialsort!(per_elf, 1:3, rev=true)
+    println("part1: $(top3[1])\npart2: $(sum(top3))")
+end
+
+solve("data/1.txt")
