0a65655e5f2efcde7b21922b083a107de75e97b3
Alexander Rauhut <alexraw@mailbox.org>
2022-12-01 18:18:44 +0100

    smaller

--- 1.R
+++ 1.R
@@ -1,12 +1,10 @@
-max_n_weight <- function(.file, n = 1) {
+solve <- function(.file) {
   calories <- scan(.file, integer(), blank.lines.skip = FALSE, quiet = TRUE)
   calories[is.na(calories)] <- 0L
   csums <- cumsum(calories)
-  sums <- diff(c(csums[1L], csums[duplicated(csums)]))
-  if (n == 1L) return(max(sums))
-  sort(sums, decreasing = TRUE)[seq_len(n)] |>
-    sum()
+  sums <- c(csums[1L], diff(csums[duplicated(csums)]))
+  c(part1 = max(sums),
+    part2 = sum(sort(sums, decreasing = TRUE)[1:3]))
 }
 
-c(part1 = max_n_weight("data/1.txt"),
-  part2 = max_n_weight("data/1.txt", 3))
+solve("data/1.txt")
