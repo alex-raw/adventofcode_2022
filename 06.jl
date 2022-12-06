89acbbcb86f74bb8f62f192009f04d31269d2d21
Alexander Rauhut <alexraw@mailbox.org>
2022-12-06 19:16:46 +0100

    added julia oneline-solution

--- 06.jl
+++ 06.jl
@@ -1,3 +1,4 @@
+# submission code
 function solve(input::String, n::Int)
     for w in (range(i, length = n) for i in eachindex(input))
         allunique(input[w]) && return last(w)
@@ -6,3 +7,7 @@ end
 
 input = readline("data/06.txt")
 println("part1 = $(solve(input, 4)), part2 $(solve(input, 14))")
+
+
+# after being reminded of findfirst
+solve(x, n) = n-1 + findfirst(w -> allunique(x[w]), map(i -> i:n-1, eachindex(x)))
