334e6e382afb1ee4dce7bed7d65acb4ed405f43a
Alexander Rauhut <alexraw@mailbox.org>
2022-12-09 15:02:42 +0100

    7 improved

--- 09.jl
+++ 09.jl
@@ -3,25 +3,19 @@ using DelimitedFiles
 function headmoves(filename)
     Δ = Dict("R" => [1, 0], "U" => [0, 1], "L" => [-1, 0], "D" => [0, -1])
     inds = [repeat([Δ[row[1]]], row[2]) for row in eachrow(readdlm(filename))]
-    return reduce(vcat, inds, init = [[0, 0]]) |> cumsum
+    return cumsum(Iterators.flatten(inds))
 end
 
-function pullrope!(moves)
-    for (i, head) in enumerate(moves[2:end])
-        moves[i + 1] = moves[i]
-        Δhead = head - moves[i]
-        if any(>(1), abs.(Δhead))
-            moves[i + 1] += sign.(Δhead)
+function pullrope!(moves; n = 1)
+    for _ in 1:n
+        for (i, head) in enumerate(moves[2:end])
+            moves[i + 1] = moves[i]
+            Δhead = head - moves[i]
+            any(>(1), abs.(Δhead)) && (moves[i + 1] += sign.(Δhead))
         end
     end
-    return moves
+    return length(unique(moves))
 end
 
-function solve(filename)
-    moves = headmoves(filename)
-    pt1 = pullrope!(moves) |> unique |> length
-    pt2 = reduce((_, _) -> pullrope!(moves), 1:8) |> unique |> length
-    println("part1 = $(pt1), part2 = $(pt2)")
-end
-
-solve("data/09.txt")
+moves = headmoves("data/09.txt")
+println("part1 = $(pullrope!(moves, n = 1)), part2 = $(pullrope!(moves, n = 8))")
