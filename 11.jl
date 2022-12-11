bbc90b5e6b7239f9404fd2737cdcf33f16d9892e
Alexander Rauhut <alexraw@mailbox.org>
2022-12-11 22:27:00 +0100

    cleanup 11

--- 11.jl
+++ 11.jl
@@ -3,14 +3,14 @@ struct Monkey
     op::Function
     val::Union{Nothing,Int}
     by::Int
-    next::Int
-    prev::Int
+    yes::Int
+    no::Int
 end
 
 function parsemonkey(m)::Monkey
     _, items, op, test... = m
     f, val = match(r"([*+]) (.*)", op).captures
-    Monkey(
+    return Monkey(
         filter!(!isnothing, tryparse.(Int, split(items, r",|: "))),
         f == "*" ? (*) : (+),
         tryparse(Int, val),
@@ -20,21 +20,19 @@ end
 
 function monkeybusiness(filename::String; k = 20, relief = 3)::Int
     monkeys = parsemonkey.(split.(split(readchomp(filename), "\n\n"), '\n'))
-    monkeyscores = zeros(Int, length(monkeys))
+    scores = zeros(Int, length(monkeys))
     lcd = prod(monkey.by for monkey in monkeys)
-    for i in 1:k
-        for (i, monkey) in enumerate(monkeys)
-            monkeyscores[i] += length(monkey.items)
-            while !isempty(monkey.items)
-                item = pop!(monkey.items)
-                level = monkey.op(item, something(monkey.val, item))
-                relief > 1 && (level ÷= relief)
-                next = level % monkey.by == 0 ? monkey.next : monkey.prev
-                push!(monkeys[next + 1].items, level % lcd)
-            end
+    for _ in 1:k, (i, monkey) in enumerate(monkeys)
+        scores[i] += length(monkey.items)
+        while !isempty(monkey.items)
+            item = pop!(monkey.items)
+            level = monkey.op(item, something(monkey.val, item))
+            relief > 1 && (level ÷= relief)
+            next = level % monkey.by == 0 ? monkey.yes : monkey.no
+            push!(monkeys[next + 1].items, level % lcd)
         end
     end
-    return prod(partialsort(monkeyscores, 1:2, rev = true))
+    return prod(partialsort(scores, 1:2, rev = true))
 end
 
 @show monkeybusiness("data/11.txt")
